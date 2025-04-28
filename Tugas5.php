30505b4d6455a57836486005b64b89b4180cab4e
al982 <aalghazali984@gmail.com>
2025-04-28 09:19:02 +0800

    Add files via upload

---
+++ Tugas5.php
@@ -0,0 +1,10 @@
+<?php
+
+$NilaiMatematika=  65;
+$NilaiBahasa =55;
+
+if ($NilaiMatematika >= 80 && $NilaiBahasa >= 70) {
+    echo "Selamat! Kamu boleh nonton bioskop akhir pekan ini!";
+} else {
+    echo "yah... belajat lagi ya, better luck next time!";
+}
