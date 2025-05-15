86729615913492aff6386178148666e78226c666
al982 <aalghazali984@gmail.com>
2025-05-15 16:01:26 +0800

    Add files via upload

---
+++ tugas12.php
@@ -0,0 +1,10 @@
+<?php
+$daftar_sepatu=["AeroStreet",
+                "Compass",
+                "Ortus",
+                "Specs"];
+                
+echo "Daftar Koleksi Sepatu <br>";
+foreach ($daftar_sepatu as $daftarsepatu) {
+    echo "-".$daftarsepatu. "<br>";
+}
